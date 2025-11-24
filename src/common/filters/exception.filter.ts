51f7728d10fffe6f0ff46cb6fdff2d5cd5385f60
saadbenz7724 <rashid786saad@gmail.com>
2025-11-24 11:18:09 +0530

    add global exception filter

---
+++ src/common/filters/exception.filter.ts
@@ -0,0 +1,27 @@
+import { ArgumentsHost, Catch, ExceptionFilter, HttpException, HttpStatus } from "@nestjs/common";
+import { Request, Response } from 'express';
+
+@Catch()
+export class HttpExceptionFilter implements ExceptionFilter{
+    catch(exception: unknown, host: ArgumentsHost) {
+        const ctx = host.switchToHttp();
+        const response = ctx.getResponse<Response>();
+        const request = ctx.getRequest<Request>();
+
+        let status = HttpStatus.INTERNAL_SERVER_ERROR;
+        let message = 'Internal Server Error';
+
+        if(exception instanceof HttpException){
+            status = exception.getStatus();
+            const res: any = exception.getResponse();
+            message = res?.message || res || exception.message;
+        }
+        return response.status(status).json({
+            success: false,
+            statusCode: status,
+            message,
+            timestamp: new Date().toISOString(),
+            path: request.url,
+        });
+    }
+}
