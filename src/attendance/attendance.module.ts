d81a8e45dc39715dc62252a34e20ba3d9127ac06
saadbenz7724 <rashid786saad@gmail.com>
2025-11-26 11:25:15 +0530

    fix issue

--- src/attendance/attendance.module.ts
+++ src/attendance/attendance.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AttendanceController } from './attendance.controller';
 import { AttendanceService } from './attendance.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Attendance } from './attendance.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Attendance])],
   controllers: [AttendanceController],
   providers: [AttendanceService]
 })
