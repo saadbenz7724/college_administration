7b20cc8d0505f68ad9204508045e263bff63e30e
saadbenz7724 <rashid786saad@gmail.com>
2025-11-27 15:41:57 +0530

    feat: enhance global exception filter with winston logging support

--- src/common/filters/exception.filter.ts
+++ src/common/filters/exception.filter.ts
@@ -1,8 +1,12 @@
-import { ArgumentsHost, Catch, ExceptionFilter, HttpException, HttpStatus } from "@nestjs/common";
+import { ArgumentsHost, Catch, ExceptionFilter, HttpException, HttpStatus, Inject } from "@nestjs/common";
 import { Request, Response } from 'express';
+import { Logger } from "winston";
 
 @Catch()
 export class HttpExceptionFilter implements ExceptionFilter{
+    constructor(
+        @Inject('winston') private readonly logger: Logger,
+    ){}
     catch(exception: unknown, host: ArgumentsHost) {
         const ctx = host.switchToHttp();
         const response = ctx.getResponse<Response>();
@@ -16,12 +20,20 @@ export class HttpExceptionFilter implements ExceptionFilter{
             const res: any = exception.getResponse();
             message = res?.message || res || exception.message;
         }
+        this.logger.error({
+            message: message,
+            method: request.method,
+            url: request.url,
+            statusCode: status,
+            timestamp: new Date().toISOString(),
+            stack: exception instanceof Error ? exception.stack: null,
+        });
         return response.status(status).json({
             success: false,
             statusCode: status,
             message,
-            timestamp: new Date().toISOString(),
             path: request.url,
+            timestamp: new Date().toISOString(),
         });
     }
 }
--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalInterceptors(new LoggingInterceptor(app.get('winston')));
   app.useGlobalPipes(new ValidationPipe({whitelist: true}))
-  app.useGlobalFilters(new HttpExceptionFilter());
+  app.useGlobalFilters(new HttpExceptionFilter(app.get('winston')));
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
